b658f4bbcee14a62f312f9ae78d8b77d08dd7247
nmelnic <en4f_mni20121991>
2017-11-17 14:14:55 +0200

    adds csv file

--- src/DML/CrudOperations/Problem_5_Bulk_Insert.java
+++ src/DML/CrudOperations/Problem_5_Bulk_Insert.java
@@ -33,7 +33,7 @@ public class Problem_5_Bulk_Insert {
             //TODO: IMPLEMENT A METHOD THAT WILL DISPLAY THE RECORDS BEFORE AND AFTER INSERT OPERATION
 
             // 3. EXECUTE UPDATE OPERATION
-            myStmt.executeUpdate("");
+            myStmt.executeUpdate("//TODO");
 
         } catch (Exception exc) {
             exc.printStackTrace();
